eac0a9aa19d43ab09674f0ee27745439b2284c11
harrydt <huydtran19@gmail.com>
2017-06-10 19:30:02 -0400

    Implement Credit function

--- src/processor.py
+++ src/processor.py
@@ -61,6 +61,8 @@ class Processor:
             return
         
         # Start charging cards
+        # "Charge" command doesn't specify which card, but it's logical to
+        # distribute the charge among cards that a user owns.
         leftover = int_amount
 
         for i in range(0, len(self.accounts[name])):
@@ -77,7 +79,18 @@ class Processor:
                 leftover = 0
 
     def _credit(self, name, amount): 
-        pass
+        '''This method is used for decreasing balance) on an account'''
+        if name not in self.accounts:
+            # User doesn't exist
+            return
+        
+        # Since "Credit" command doesn't specify which card, and there's
+        # a chance that a user would want negative balance on certain card,
+        # This credit will be applied to the first card for now
+        # TODO revisit implementation
+        if self.accounts[name]:
+            self.accounts[name][0]['balance'] -= int(amount[1:])
+
 
     def _verify_unique_card_number(self, card_number):
         '''Naive implementation to check if card number is already in shelve'''
